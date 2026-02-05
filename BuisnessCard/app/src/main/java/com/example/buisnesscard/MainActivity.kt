a81f0e9d7117c68d28c304f33194a3cd0184e48f
RSMSPN01 <praveennikhilbrothers@gmail.com>
2026-02-05 15:49:08 +0530

    Errors fixed in the code and Last project is now finally completed

--- BuisnessCard/app/src/main/java/com/example/buisnesscard/MainActivity.kt
+++ BuisnessCard/app/src/main/java/com/example/buisnesscard/MainActivity.kt
@@ -1,6 +1,4 @@
 package com.example.buisnesscard
-
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,13 +9,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Call
-import androidx.compose.material.icons.rounded.Email
-import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -32,7 +29,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.buisnesscard.ui.theme.BuisnessCardTheme
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,12 +36,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             BuisnessCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    DisplayDetails(
+                    ShowDetails(
                         "Praveen Kumar",
-                        "Android developer Extraordinary",
-                        modifier = Modifier.padding(innerPadding).fillMaxSize()
-                    )
-                    ContactDetails(
+                        "Android Developer",
                         91,
                         123456789,
                         "pk@gmail.com",
@@ -58,87 +51,116 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ContactDetails(
-    countryCode: Int,
-    mobileNumber: Long,
+fun ShowDetails(
+    name: String,
+    role: String,
+    code: Int,
+    mobile: Int,
     email: String,
     modifier: Modifier = Modifier
 ) {
     Column(
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom,
         modifier = modifier
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(
-                5.dp,
-
-            )
-        ) {
-            Icon(
-                Icons.Rounded.Call,
-                contentDescription = "null",
-
-                )
-            Text(
-                text = "+$countryCode $mobileNumber",
-                color = Color(0xFF295e44)
-            )
-        }
-        Row(modifier = Modifier.padding(vertical = 6.dp),
-            verticalAlignment = Alignment.CenterVertically) {
-            Icon(
-                Icons.Rounded.Share,
-                contentDescription = "null"
-            )
-            Text(
-                text = "@Android Dev",
-                color = Color(0xFF295e44)
-            )
-        }
-        Row(modifier = Modifier.padding(5.dp)) {
-            Icon(
-                Icons.Rounded.Email,
-                contentDescription = "null"
-            )
-            Text(
-                text = email,
-                color = Color(0xFF295e44)
-            )
-        }
+        TopDetails(
+            name,
+            role,
+            Modifier.weight(1f)
+        )
+        BottomDetail(
+            code,
+            mobile,
+            email,
+            Modifier
+        )
     }
 }
 
 @Composable
-fun DisplayDetails(
+fun TopDetails(
     name: String,
-    jobRole: String,
+    role: String,
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier,
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
     ) {
         Image(
             painter = painterResource(R.drawable.android_logo),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
-                .height(150.dp)
-                .width(150.dp)
-                .background(color = Color(0xFF182c36))
+                .size(200.dp)
+                .background(color = (Color.Black))
         )
         Text(
             text = name,
-            fontSize = 35.sp,
-            modifier = Modifier.padding(10.dp)
+            fontSize = 25.sp,
+            modifier = Modifier.padding(top = 10.dp)
         )
         Text(
-            text = jobRole,
-            color = Color(0xFF295e44)
+            text = role,
+            color = (Color.Green)
         )
+    }
+}
+
+@Composable
+fun BottomDetail(
+    code: Int,
+    mobile: Int,
+    email: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start,
+        modifier = modifier.padding(10.dp)
+    ) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(3.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Call,
+                contentDescription = "Call Button",
+                modifier = Modifier.padding(end = 15.dp)
+            )
+            Text(
+                text = "+" + code + " " + mobile
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(3.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Share,
+                contentDescription = "Share Button",
+                modifier = Modifier.padding(end = 15.dp)
+            )
+            Text(
+                text = "@Android Dev"
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(3.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Email,
+                contentDescription = "Email Button",
+                modifier = Modifier.padding(end = 15.dp)
+            )
+            Text(
+                text = email
+            )
+        }
     }
 }
 
@@ -146,7 +168,9 @@ fun DisplayDetails(
 @Composable
 fun GreetingPreview() {
     BuisnessCardTheme {
-        ContactDetails(
+        ShowDetails(
+            "Praveen Kumar",
+            "Developer",
             91,
             123456789,
             "pk@gmail.com",
